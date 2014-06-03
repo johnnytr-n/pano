d359c25e6f3729a052097dd87c71b73113158992
ifrit117 <ifrit117@gmail.com>
2014-06-03 11:55:48 -0700

    cleaned up pano file

--- pano.py
+++ pano.py
@@ -1,4 +1,4 @@
-# Johnny Tran - ECE 118
+# Johnny Tran - ECE 118 - SP14
 import time
 import math
 import smbus
@@ -7,55 +7,27 @@ import RPi.GPIO as GPIO
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 # Params
-default_start = 180
-SHUTTER = 23
-FOCUS = 24
-shutter_stall = .2
-rotation = default_start
-str1 = "Pano Angle:\n"
-OP_SUBMIT = 0x03
-OP_RIGHT = 'R'
-OP_LEFT = 'L'
+default_start = 180			# default start position for angle
+default_interval = 36 		# default interval angle
+SHUTTER = 23				# shutter GPIO pin		
+FOCUS = 24					# focus GPIO pin
+shutter_stall = .2 			# time in seconds to keep GPIO pin on high
+rotation = default_start	# default rotation set to default start position
+shutter_sentinel = "X"		# sentinel value for shutter release
+halt_sentinel = "Z"			# sentinel value to halt watching serial comm
+str1 = "Pano Angle:\n"		
+OP_SUBMIT = 'S'			# serial code to send for panoroma execution
+OP_RIGHT = 'R'				# serial code to send for right rotation
+OP_LEFT = 'L'				# serial code to send for left rotation
 OP_JNT = '0'
-
-# sends data on to the I2C bus
-def send(val):
-	bus.write_byte(uno_addr, val)
-	return -1
-
-# receives data off to the I2C bus
-def receive():
-	num = bus.read_byte(uno_addr)
-	return num
 
 # returns number of 36 degree intervals in user designated range
 def calc_interval(user_deg):
-	if 0<= user_deg < 36:
+	if 0<= user_deg < default_interval:
 		return 1
-	elif 36 <= user_deg < 324:
-		return int(math.ceil(user_deg/36))
-OP_SUBMIT = 0x03
-OP_RIGHT = 'R'
-OP_LEFT = 'L'
-OP_JNT = '0'
-
-# sends data on to the I2C bus
-def send(val):
-	bus.write_byte(uno_addr, val)
-	return -1
-
-# receives data off to the I2C bus
-def receive():
-	num = bus.read_byte(uno_addr)
-	return num
-
-# returns number of 36 degree intervals in user designated range
-def calc_interval(user_deg):
-	if 0<= user_deg < 36:
-		return 1
-	elif 36 <= user_deg < 324:
-		return int(math.ceil(user_deg/36))+1
-	elif 324 <= user_deg:
+	elif default_interval <= user_deg < 360-default_interval:
+		return int(math.ceil(user_deg/default_interval))+1
+	elif 360-default_interval <= user_deg:
 		return 9
 	else:
 		err()
@@ -63,7 +35,7 @@ def calc_interval(user_deg):
 
 #turns backlight red if there is an error
 def err():
-	GPIO.cleanup()
+	GPIO.cleanup()			# reset GPIO pins
 	lcd.backlight(lcd.RED)
 
 # updates display with current rotation selection
@@ -74,53 +46,53 @@ def update_rotation(curr_rotation, diff):
 		new_rotation = 0
 	elif curr_rotation == 0 and diff == -1.8:
 		new_rotation = 360
+
 	if new_rotation < 0:
 		new_rotation = 360
+
 	if new_rotation != 360:
 		new_rotation %= 360
 
 	update_display(new_rotation)
 	return new_rotation
 
-#
+# updates angle display
 def update_display(message):
 	display_str = str1 + str.format('{0:.1f}', message) + " degrees"
 	lcd.clear()
 	lcd.message(display_str)	
 
-# creates an instruction to send to Arduino
+# calculates information to send to Arduino for panorama execution
 def submit(rotation_var, op_code):
 	instruction = 0;
 	if op_code == OP_SUBMIT:
 		instruction = str(calc_interval(rotation_var))	
 	else:
 		instruction = op_code
-	#print "send instr ", bin(instruction)
-	#print str(instruction)i
-	print str(instruction)
+	print "Pan " + rotation_var " degrees, in " + str(instruction) + "intervals"
 	uno_serial.write(instruction)
 	
-# use GPIO to release camera shutter
+# use GPIO to release DSLR shutter
 def release_shutter():
-	GPIO.output(SHUTTER, GPIO.HIGH)
-	time.sleep(shutter_stall)
-	GPIO.output(SHUTTER, GPIO.LOW)
+	GPIO.output(SHUTTER, GPIO.HIGH)	# set logic to high 
+	time.sleep(shutter_stall)		# give DSLR time to register shutter
+	GPIO.output(SHUTTER, GPIO.LOW)	# set logic to low
 
-#focus camera
+# focus/wake DSLR
 def focus():
-	GPIO.output(FOCUS, GPIO.HIGH)
-	time.sleep(shutter_stall)
-	GPIO.output(FOCUS, GPIO.LOW)
+	GPIO.output(FOCUS, GPIO.HIGH)	# set logic to high
+	time.sleep(shutter_stall)		# give DSLR time to register
+	GPIO.output(FOCUS, GPIO.LOW)	# set logic to low
 
-# displays pano text to user, waits on response from Arduino
-def pano_display(submitted_rotation):
+# poll serial communication for shutter release
+def pano_shutter(submitted_rotation):
 	uno_message = 0
 	while True:
-		uno_message = uno_serial.read(1)	# serial read 1 character
+		uno_message = uno_serial.read(1)	# read 1 character from serial comm
 		print uno_message					# print serial stream
-		if uno_message == "X":				# signal to release shutter
+		if uno_message == shutter_sentinel:	# signal to release shutter
 			release_shutter()
-		elif uno_message == "Z":			# signal to stop watching line
+		elif uno_message == halt_sentinel:	# signal to stop
 			break	
 	lcd.backlight(lcd.ON)
 	for i in range(5,-1,-1):
@@ -130,16 +102,18 @@ def pano_display(submitted_rotation):
 		
 
 ##############################################################
-
+# open serial communication through UART
 uno_serial = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 uno_serial.open()
 
+# initialize GPIO and setup output pins for DSLR communication
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SHUTTER, GPIO.OUT)
 GPIO.setup(FOCUS, GPIO.OUT)
 GPIO.output(SHUTTER, GPIO.LOW)
 GPIO.output(FOCUS, GPIO.LOW)
 
+# initialize LCD object and display splash screen
 lcd = Adafruit_CharLCDPlate()
 lcd.clear()
 lcd.backlight(lcd.ON)
@@ -151,13 +125,10 @@ btn = ((lcd.LEFT  , 1),
        (lcd.RIGHT , 2),
        (lcd.SELECT, 3))
 
-#bus = smbus.SMBus(1)
-uno_addr = 0x04			# I2C address of Arduino Uno R3
 time.sleep(2)
 
 update_display(rotation)
 
-prev = -1
 try:
 	while True:
 		time.sleep(0.2) #sleep for 200ms to prevent key bouncing
@@ -167,28 +138,28 @@ try:
 					rotation = update_rotation(rotation, b[1])
 					print "pressed ", rotation
 					break
+				# user specified alignment and repositioning of motor shaft
+				#rotate	motor over 10 steps left or right
 				elif b[0] == lcd.LEFT:
 					submit(0, OP_LEFT)
 					break
 				elif b[0] == lcd.RIGHT:
-					#alight and reposition motor shaft
-					#rotate	motor over 10 steps left or right
 					submit(0, OP_RIGHT)	
 					break
+				# user submission to execute panorama
 				elif b[0] == lcd.SELECT:	
-					#submit rotation and execute pano
-					focus()
+					focus()						# wake camera and focus
 					lcd.clear()
 					lcd.backlight(lcd.VIOLET)
 					lcd.message("Executing Pano!\n Please wait...")
-					submit(rotation, OP_SUBMIT)
-					pano_display(rotation)
-					rotation = default_start
+					submit(rotation, OP_SUBMIT)	# submit pano intent
+					pano_shutter(rotation)		# poll comm for shutter release
+					rotation = default_start	# set to default
 					update_display(rotation)
 				break
-except KeyboardInterrupt:
-	print "You have exited Pano"
-except:
+except KeyboardInterrupt:				# exit on user interrupt
+	print "\nYou have exited Pano"
+except:									# err gracefully
 	err()
-finally:
+finally:								# clean up and reset GPIO on exit
 	GPIO.cleanup()
